571e8c4ee88edc858f21bce141a1184b68834e32
ryleu <69326171+ryleu@users.noreply.github.com>
2025-03-29 14:33:16 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Configuration flake for ryleu's desktop";
+
+  # shared configuration is stored in configuration.nix, while individual configs are in the files
+  # named for the device's hostname. eg: rectangle uses configuration.nix and rectangle.nix
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs_unstable, ... }@inputs: {
+    nixosConfigurations = let
+        system = "x86_64-linux";
+        baseModules = [
+          ./configuration.nix
+        ];
+    in {
+      # default configuration
+      default = nixpkgs.lib.nixosSystem {
+        modules = baseModules;
+      };
+
+      # rectangle's nixos configuration
+      rectangle = nixpkgs.lib.nixosSystem {
+        modules = baseModules ++ [
+          ./rectangle.nix
+        ];
+      };
+
+      # mathrock's nixos configuration
+      nixosConfigurations.mathrock = nixpkgs.lib.nixosSystem {
+        modules = baseModules;
+      };
+    };
+  };
+}
+
---
+++ rectangle.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  networking.hostName = "rectangle";
+
+  # amdgpu shit
+  services.xserver = {
+    videoDrivers = [ "amdgpu" ];
+  };
+  boot.initrd.kernelModules = [ "amdgpu" ];
+
+  # additional storage
+  fileSystems = {
+    "/mnt/hdd" = {
+      device = "/dev/disk/by-uuid/1400b78a-afe6-4764-ab36-e23cb033455c";
+      fsType = "btrfs";
+      options = [
+        "defaults"
+        "nofail"
+      ];
+    };
+    "/mnt/ssd" = {
+      device = "/dev/disk/by-uuid/09ba989e-ef6a-413b-bb4c-21923d441b8b";
+      fsType = "btrfs";
+      options = [
+        "defaults"
+        "nofail"
+      ];
+    };
+  };
+}
