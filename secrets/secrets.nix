46002ed5ede1416eac0125e7115b6fd68df82dc8
ryleu <69326171+ryleu@users.noreply.github.com>
2025-04-16 02:52:43 -0500

    add barely better pubkey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,8 +2,9 @@ let
   users = import ./authorized_keys;
 
   rectangle = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJRbbUTDpqCJcomV4S3K63TbF9aV8MpxVQ0fjot98SDs root@rectangle";
+  barely-better = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAWotWG7ZQEFVI5qmnsK/VAaqEqaJlhV60NYz4iJACJq root@barely-better";
 
-  systems = [ rectangle ];
+  systems = [ rectangle barely-better ];
 in
 {
   "ripi-key.age".publicKeys = users ++ systems;
