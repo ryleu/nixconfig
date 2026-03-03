d8b661a6552cf4ec14369ab75b222378607aa074
ryleu <69326171+ryleu@users.noreply.github.com>
2026-03-02 22:07:45 -0600

    and now i can accept credit card payments (almost)

--- modules/hardware/ethernet-port/default.nix
+++ modules/hardware/ethernet-port/default.nix
@@ -5,6 +5,7 @@
     ../laptop.nix
     ./hardware-configuration.nix
     ./fingerprint.nix
+    ./smart-card.nix
 
     # technically i have a gen 6, but this is close enough
     inputs.hardware.nixosModules.lenovo-thinkpad-p14s-amd-gen5
