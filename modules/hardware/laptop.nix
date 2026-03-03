cc7ee25fa1891f22eda8242cb8cf9c73170e6def
ryleu <69326171+ryleu@users.noreply.github.com>
2026-03-02 20:37:24 -0600

    now we have cool kernel

--- modules/hardware/laptop.nix
+++ modules/hardware/laptop.nix
@@ -1,9 +1,11 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
     inputs.hardware.nixosModules.common-pc-ssd
   ];
+
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 
   services.logind = {
     settings.Login = {
