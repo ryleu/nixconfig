0dcf0ae6cef9b9970954bd2491f3cf0f87a08294
ryleu <69326171+ryleu@users.noreply.github.com>
2026-03-02 21:26:40 -0600

    and now we are fingering

--- modules/hardware/ethernet-port/default.nix
+++ modules/hardware/ethernet-port/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ../laptop.nix
     ./hardware-configuration.nix
+    ./fingerprint.nix
 
     # technically i have a gen 6, but this is close enough
     inputs.hardware.nixosModules.lenovo-thinkpad-p14s-amd-gen5
