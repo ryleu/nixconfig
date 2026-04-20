92ecc67e807338361734d790cbceed3c26a0fc12
ryleu <69326171+ryleu@users.noreply.github.com>
2026-04-20 12:28:27 -0500

    fix: reinstall nixos on ethernet-port to get btrfs

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -16,6 +16,7 @@ in
   imports = [
     inputs.home-manager.nixosModules.home-manager
     {
+      home-manager.backupFileExtension = "bak";
       home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.extraSpecialArgs = {
