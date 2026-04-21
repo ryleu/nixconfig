e1b99cd6b383b4869ee5f54057ba3768e090d8a8
ryleu <69326171+ryleu@users.noreply.github.com>
2026-04-21 13:38:31 -0500

    feat: add a password store to git

--- modules/home-manager/shared/git.nix
+++ modules/home-manager/shared/git.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ config, pkgs, ... }:
 {
   programs = {
     git = {
       enable = true;
-      settings.init.defaultBranch = "main";
+      settings = {
+        init.defaultBranch = "main";
+        credential = {
+          helper = "${pkgs.git-credential-manager}/bin/git-credential-manager";
+          credentialStore = "gpg";
+        };
+      };
     };
 
     difftastic = {
@@ -15,5 +21,19 @@
     };
 
     mergiraf.enable = true;
+  };
+
+  programs.gpg.enable = true;
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 3600;
+    maxCacheTtl = 86400;
+    pinentry.package = pkgs.pinentry-curses;
+  };
+
+  programs.password-store = {
+    enable = true;
+    settings.PASSWORD_STORE_DIR = "${config.home.homeDirectory}/.local/share/pass";
   };
 }
--- modules/home-manager/wyleu/git.nix
+++ modules/home-manager/wyleu/git.nix
@@ -1,16 +1,14 @@
 { ... }:
 {
-  programs = {
-    git = {
-      settings = {
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        user = {
-          signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3ETWWGQPgH2cNSFvdyo6/kA9Xni/cebIEz8mXqqzYD";
-          email = "riley.mclain@watts.ai";
-          name = "riley.mclain";
-        };
-      };
+  programs.git.settings = {
+    commit.gpgsign = true;
+    gpg.format = "ssh";
+    user = {
+      signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3ETWWGQPgH2cNSFvdyo6/kA9Xni/cebIEz8mXqqzYD";
+      email = "riley.mclain@watts.ai";
+      name = "riley.mclain";
     };
+
+    credential.gpgEncryptionKey = "7D8490DACBD93D7F15B808EA3E3DB6CA7E43CF7E";
   };
 }
