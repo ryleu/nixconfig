a748a3f018229a5e090a5bb78fa379267fb1e6d7
ryleu <69326171+ryleu@users.noreply.github.com>
2026-05-13 15:36:24 -0500

    feat(nixos): add auto gc and optimization

---
+++ modules/nixos/gc.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  nix = {
+    optimise = {
+      automatic = true;
+      dates = [ "09:00" ];
+    };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 90d";
+    };
+  };
+}
