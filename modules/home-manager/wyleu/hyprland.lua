69b92e7dc6c8841b6b57cab3767ac5994ea9dd91
ryleu <69326171+ryleu@users.noreply.github.com>
2026-06-08 09:25:39 -0500

    feat: set up new monitors

--- modules/home-manager/wyleu/hyprland.lua
+++ modules/home-manager/wyleu/hyprland.lua
@@ -1,5 +1,8 @@
 -- wyleu monitor + workspace assignments
+-- integrated monitor
 hl.monitor({ output = "eDP-1", mode = "2880x1800", position = "0x1080", scale = 1.5 })
+
+-- horizontal monitor on top
 hl.monitor({
     output = "desc:Dell Inc. DELL P2722H FRL6293",
     mode = "1920x1080",
@@ -7,7 +10,22 @@ hl.monitor({
     scale = 1,
 })
 hl.monitor({
+    output = "desc:Dell Inc. DELL P2722H H2L6293",
+    mode = "1920x1080",
+    position = "0x0",
+    scale = 1,
+})
+
+-- vertical monitor on the right
+hl.monitor({
     output = "desc:Dell Inc. DELL P2722H 6QL6293",
+    mode = "1920x1080",
+    position = "1920x0",
+    scale = 1,
+    transform = 3,
+})
+hl.monitor({
+    output = "desc:Dell Inc. DELL P2722H 53L6293",
     mode = "1920x1080",
     position = "1920x0",
     scale = 1,
