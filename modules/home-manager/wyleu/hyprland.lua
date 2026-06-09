b05f39e9cfa8fd3bf74d0f0219e45acd8f5c5fef
ryleu <69326171+ryleu@users.noreply.github.com>
2026-06-09 09:29:14 -0500

    fix: workspaces stick to the proper wyleu monitors

--- modules/home-manager/wyleu/hyprland.lua
+++ modules/home-manager/wyleu/hyprland.lua
@@ -1,7 +1,7 @@
 -- wyleu monitor + workspace assignments
 -- integrated monitor
 hl.monitor({ output = "eDP-1", mode = "2880x1800", position = "0x1080", scale = 1.5 })
-hl.workspace_rule({ workspace = "1", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", default = true, persistent = true })
 
 -- horizontal monitor on top
 horizontals = { "desc:Dell Inc. DELL P2722H FRL6293", "desc:Dell Inc. DELL P2722H H2L6293" }
@@ -12,7 +12,7 @@ for _, display in ipairs(horizontals) do
         position = "0x0",
         scale = 1,
     })
-    hl.workspace_rule({ workspace = "6", monitor = display })
+    hl.workspace_rule({ workspace = "6", monitor = display, default = true, persistent = true })
 end
 
 -- vertical monitor on the right
@@ -25,5 +25,5 @@ for _, display in ipairs(verticals) do
         scale = 1,
         transform = 3,
     })
-    hl.workspace_rule({ workspace = "11", monitor = display })
+    hl.workspace_rule({ workspace = "11", monitor = display, default = true, persistent = true })
 end
