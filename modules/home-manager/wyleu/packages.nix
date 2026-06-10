93c28ecbbb9c302d29b1567d507de58376eb63be
ryleu <69326171+ryleu@users.noreply.github.com>
2026-06-10 12:26:16 -0500

    feat(work): remove claude desktop as i no longer need it

--- modules/home-manager/wyleu/packages.nix
+++ modules/home-manager/wyleu/packages.nix
@@ -6,32 +6,6 @@ let
     inherit system;
     config.allowUnfree = true;
   };
-
-  # claude-desktop
-  claudeDesktopSrc = inputs.claude-desktop;
-  patchy-cnb = pkgs.callPackage "${claudeDesktopSrc}/pkgs/patchy-cnb.nix" { };
-  claude-desktop = pkgs.callPackage "${claudeDesktopSrc}/pkgs/claude-desktop.nix" {
-    inherit patchy-cnb;
-    nodePackages = { inherit (pkgs) asar; };
-  };
-  claude-desktop-with-fhs = pkgs.buildFHSEnv {
-    name = "claude-desktop";
-    targetPkgs =
-      pkgs: with pkgs; [
-        docker
-        glibc
-        openssl
-        nodejs
-        uv
-      ];
-    runScript = "${claude-desktop}/bin/claude-desktop";
-    extraInstallCommands = ''
-      mkdir -p $out/share/applications
-      cp ${claude-desktop}/share/applications/claude.desktop $out/share/applications/
-      mkdir -p $out/share/icons
-      cp -r ${claude-desktop}/share/icons/* $out/share/icons/
-    '';
-  };
 in
 {
   home.packages = with pkgs; [
@@ -43,8 +17,5 @@ in
     master_pkgs.claude-code
     docker-buildx # better docker builder
     master_pkgs.liteparse
-
-    # apps
-    claude-desktop-with-fhs
   ];
 }
