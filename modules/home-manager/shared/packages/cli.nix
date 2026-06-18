0f672e247e6ae86939977c6dccf51530a86b9542
ryleu <69326171+ryleu@users.noreply.github.com>
2026-06-17 23:51:15 -0500

    fix: Wi-Fi -> WiFi

--- modules/home-manager/shared/packages/cli.nix
+++ modules/home-manager/shared/packages/cli.nix
@@ -32,7 +32,7 @@
 
   xdg.desktopEntries = {
     spotify-player = mkTuiLauncher "Spotify" pkgs.spotify-player;
-    impala = mkTuiLauncher "Wi-Fi" pkgs.impala;
+    impala = mkTuiLauncher "WiFi" pkgs.impala;
     bluetui = mkTuiLauncher "Bluetooth" pkgs.bluetui;
     btop = mkTuiLauncher "System Monitor" pkgs.btop-rocm;
   };
