26d4160e89dd18c58bea7dfc807bd6d77bf56a0a
ryleu <69326171+ryleu@users.noreply.github.com>
2026-06-21 19:12:12 -0500

    feat: update typst

--- modules/home-manager/shared/packages/typst.nix
+++ modules/home-manager/shared/packages/typst.nix
@@ -1,6 +1,14 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
+let
+  system = pkgs.stdenv.hostPlatform.system;
+
+  master_pkgs = import inputs.master_pkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+in
 {
-  home.packages = with pkgs; [
+  home.packages = with master_pkgs; [
     typst
     typstyle
     typst-live
